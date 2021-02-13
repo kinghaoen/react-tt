7d80af5b011eb62afa445bce9d63956114dff300
kinghaoen <kinghaoen@gmail.com>
2021-02-13 19:38:14 +0800

    封装案例

--- react-staging/09_NavLink的使用/src/App.jsx
+++ react-staging/09_NavLink的使用/src/App.jsx
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import Home from './pages/home';
 import About from './pages/about';
-import {Route} from 'react-router-dom';
-import MyNavLink from "./components/MyNavLink";
+import {Route, NavLink} from 'react-router-dom';
 
 import './App.css'
 
@@ -21,12 +20,8 @@ class App extends Component {
                             <div className="list-group">
                                 {/*<a className="list-group-item" href="./about.html">About</a>*/}
                                 {/*<a className="list-group-item active" href="./home.html">Home</a>*/}
-
-                                {/*<NavLink activeClassName="myNavLink" className="list-group-item" to="/about">About</NavLink>*/}
-                                {/*<NavLink activeClassName="myNavLink" className="list-group-item" to="/home">Home</NavLink>*/}
-
-                                <MyNavLink to="/about" >About</MyNavLink>
-                                <MyNavLink to="/home" >Home</MyNavLink>
+                                <NavLink activeClassName="myNavLink" className="list-group-item" to="/about">About</NavLink>
+                                <NavLink activeClassName="myNavLink" className="list-group-item" to="/home">Home</NavLink>
                             </div>
                         </div>
                         <div className="col-xs-6">
