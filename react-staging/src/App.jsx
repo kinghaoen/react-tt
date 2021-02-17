58f50104b7fd353fa49f8a610d2690ddb8cf649f
kinghaoen <kinghaoen@gmail.com>
2021-02-17 23:38:16 +0800

    antd按需引入

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import {Button} from "antd";
 
 class App extends Component {
     render() {
         return (
             <div>
-
+                <Button type="primary">Primary Button</Button>
+                <Button>Default Button</Button>
+                <Button type="dashed">Dashed Button</Button>
+                <br />
+                <Button type="text">Text Button</Button>
+                <Button type="link">Link Button</Button>
             </div>
         );
     }
